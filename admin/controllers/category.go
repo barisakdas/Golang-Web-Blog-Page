5bc478c97646ad2f679f55df8b149dfe78db3578
barisakdas <39412598+barisakdas@users.noreply.github.com>
2021-07-08 14:32:40 +0300

    updated

--- admin/controllers/category.go
+++ admin/controllers/category.go
@@ -4,75 +4,85 @@ import (
 	"blog/admin/helpers"
 	"blog/admin/log"
 	"blog/admin/models"
-	"github.com/julienschmidt/httprouter"
+	"fmt"
 	"html/template"
 	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type Category struct {}
+type Category struct{}
 
-func (category Category) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params)  {
-	helpers.CheckPageSession(w,r)
+func (category Category) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	helpers.CheckPageSession(w, r)
 
-	view, err := template.ParseFiles(helpers.Include("category/list/")...)	// Buradaki `...` diziyi sanki string elemanları yan yana vermiş gibi gösterecek.
+	view, err := template.New("index").Funcs(template.FuncMap{
+		"getCategory":func(categoryId int) string {
+			return models.Category{}.Get(categoryId).Name
+		},
+		"getDate" : func(t time.Time) string {
+			return fmt.Sprintf("%02d.%02d.%d",t.Day(),int(t.Month()),t.Year())
+		},
+	}).ParseFiles(helpers.Include("category/list/")...)
 	if err != nil {
-		log.LogJson("Error","Category","Index","Could not convert html files for go to read.",err.Error())
+		log.LogJson("Error", "Category", "Index", "Could not convert html files for go to read.", err.Error())
 		return
 	}
 
-	data:= make(map[string]interface{})
-	data["categories"] =models.Category{}.GetAll()
-	data["alerts"] = helpers.GetAlert(w,r)
-	view.ExecuteTemplate(w,"index",data)
+	data := make(map[string]interface{})
+	data["categories"] = models.Category{}.GetAll()
+	data["alerts"] = helpers.GetAlert(w, r)
+	view.ExecuteTemplate(w, "index", data)
 }
 
-func (category Category) AddNewCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params)  {
-	helpers.CheckPageSession(w,r)
+func (category Category) AddNewCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	helpers.CheckPageSession(w, r)
 
-	view, err := template.ParseFiles(helpers.Include("category/add/")...)	// Buradaki `...` diziyi sanki string elemanları yan yana vermiş gibi gösterecek.
+	view, err := template.ParseFiles(helpers.Include("category/add/")...) // Buradaki `...` diziyi sanki string elemanları yan yana vermiş gibi gösterecek.
 	if err != nil {
-		log.LogJson("Error","Category","Add","Could not convert html files for go to read.",err.Error())
+		log.LogJson("Error", "Category", "Add", "Could not convert html files for go to read.", err.Error())
 		return
 	}
-	view.ExecuteTemplate(w,"index",nil)
+	view.ExecuteTemplate(w, "index", nil)
 }
 
-func (category Category) AddCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params)  {
+func (category Category) AddCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	categoryName := r.FormValue("category-name")
 	models.Category{
 		Name: categoryName,
 	}.Add()
 
-	helpers.SetAlert(w,r,"Kayıt Başarılı!!")
-	http.Redirect(w,r,"/admin/categories",http.StatusSeeOther)
+	helpers.SetAlert(w, r, "Kayıt Başarılı!!")
+	http.Redirect(w, r, "/admin/categories", http.StatusSeeOther)
 }
 
-func (category Category) DeleteCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params){
-	cat:= models.Category{}.Get(params.ByName("id"))
+func (category Category) DeleteCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	cat := models.Category{}.Get(params.ByName("id"))
 	cat.Delete()
-	http.Redirect(w,r,"/admin",http.StatusSeeOther)
+	http.Redirect(w, r, "/admin/categories", http.StatusSeeOther)
 }
 
-func (category Category) UpdateCategoryIndex(w http.ResponseWriter, r *http.Request, params httprouter.Params){
-	helpers.CheckPageSession(w,r)
+func (category Category) UpdateCategoryIndex(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	helpers.CheckPageSession(w, r)
 
-	view, err := template.ParseFiles(helpers.Include("category/edit/")...)	// Buradaki `...` diziyi sanki string elemanları yan yana vermiş gibi gösterecek.
+	view, err := template.ParseFiles(helpers.Include("category/edit/")...) // Buradaki `...` diziyi sanki string elemanları yan yana vermiş gibi gösterecek.
 	if err != nil {
-		log.LogJson("Error","Category","Update","Could not convert html files for go to read.",err.Error())
+		log.LogJson("Error", "Category", "Update", "Could not convert html files for go to read.", err.Error())
 		return
 	}
 
-	data:= make(map[string]interface{})
+	data := make(map[string]interface{})
 	data["category"] = models.Category{}.Get(params.ByName("id"))
-	view.ExecuteTemplate(w,"index",data)
+	view.ExecuteTemplate(w, "index", data)
 }
 
-func (category Category) UpdateCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params){
+func (category Category) UpdateCategory(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	cat := models.Category{}.Get(params.ByName("id"))
 	categoryName := r.FormValue("category-name")
 
 	cat.Update(models.Category{
 		Name: categoryName,
 	})
-	http.Redirect(w,r,"/admin/categories",http.StatusSeeOther)
+	http.Redirect(w, r, "/admin/categories", http.StatusSeeOther)
 }
